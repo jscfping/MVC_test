4cf16f37ab547f3e9000dd99db942bca3a460763
jscfping <jscfping@gmail.com>
2020-02-20 23:01:48 +0800

    test view's model function

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -3,29 +3,30 @@ using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
-    public class HelloWorlddController : Controller
+    public class HelloWorldController : Controller
     {
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            //return "This is my default action...";View()
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }
 
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes)
+        //public string Welcome(string name, int numTimes = 1)//1 means default
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
